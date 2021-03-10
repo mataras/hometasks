3ad615819dfc49d77cd34f37651d06f1251d39bd
Vladislav Kutas <kutas.uladzislau@gmail.com>
2021-03-10 15:44:11 +0300

    Add the Trap to remove ./test.json file.

--- github_script.sh
+++ github_script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# The Trap to delete ./test.json on exit from the script 
+# even if an error occurred during execution or a user aborts the script manually
+trap "rm -f ./test.json" EXIT SIGINT SIGQUIT SIGKILL SIGTERM
+
+# If there is 1 or more argument and first argument is not empty - treat first argument as GitHub repo URL
+# Otherwise use default values
 if [ ${#@} -ge 1 ] && [ -n $1 ]; 
   then 
     user_name=$(echo "$1" | cut -d'/' -f4);
@@ -12,6 +18,8 @@ fi
 api_pulls_url="https://api.github.com/repos/${user_name}/${repo_name}/pulls?per_page=100&page=1"
 
 curl -s  -H "Accept: application/vnd.github.v3+json"   "$api_pulls_url" > ./test.json
+
+
 
 if [ -z "$(jq '.[].number' test.json)" ]; then
   printf "There is no pull requests in this repo"
@@ -31,5 +39,3 @@ else
   jq '.[] | (.number|tostring) + " " +  .user.login' test.json | sed 's/\"//g' | sort | awk '{print "User" , $2, "with PR number" , $1}' 
   
 fi
-
-rm test.json
